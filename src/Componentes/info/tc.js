a032f6f97dcada6c7e61a20782d4fe2f563af50e
SzucsJuan21 <juanszucs.dev@hotmail.com>
2022-09-05 21:07:54 -0300

    Cambios en banner 2 y banner 3

--- src/Componentes/info/tc.js
+++ src/Componentes/info/tc.js
@@ -11,15 +11,17 @@ function Tc() {
 
       <div className="imagen-tc">
         <img className="imagen-tc"
-          src={require("./canasta.png")}
+          src={require("./box-salada.jpg")}
           alt="imagen-tc" />
       </div>
 
       <div className="contenedor-tc">
-        <h1 className="titulo-tc">Surprise Someone </h1>
-        <h2 className="subtitulo-tc">Working from Home</h2>
-        <p className="texto-tc">Do you have a colleague or family member working hard at home? Our Working From Home box of snacks and drinks is the perfect way to put a smile on their face, delivered direct to their door.
-          To buy for a large group of people, get in touch and we can discuss bespoke bundles and personalised messaging.</p>
+        <h1 className="titulo-tc">¡SORPRENDE!</h1>
+        {/* <h2 className="subtitulo-tc">Working from Home</h2> */}
+        {/* <p className="texto-tc">Do you have a colleague or family member working hard at home? Our Working From Home box of snacks and drinks is the perfect way to put a smile on their face, delivered direct to their door.
+          To buy for a large group of people, get in touch and we can discuss bespoke bundles and personalised messaging.</p> */}
+          <p className="texto-tc">Tenés algún familiar, amigo/a o pareja a quién te gustaría sorprender? En Premium Bakery te ofrecemos las box más originales para que puedas 
+          alegrarle el día a esa persona especial. Y lo mejor de todo, te lo llevamos a la puerta de tu casa!</p>
       </div>
 
     </div>
