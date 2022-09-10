2136e4734745a2fff078db19ded6cabac005df39
SzucsJuan21 <juanszucs.dev@hotmail.com>
2022-09-10 16:25:02 -0300

    Cambién la fuente de algunas palabras

--- src/Componentes/CardsInfo/CardsInfo.js
+++ src/Componentes/CardsInfo/CardsInfo.js
@@ -1,21 +1,21 @@
 const Cards = (props) => {
     return (
-          <figure style={s.estilofig}>
-              <div style={s.estiloicon}><ion-icon name="telescope-outline"></ion-icon></div>
-              {props.children}
-          </figure>
+        <figure style={s.estilofig}>
+            <div style={s.estiloicon}><ion-icon name="telescope-outline"></ion-icon></div>
+            {props.children}
+        </figure>
     )
-  }
-  
-  export default Cards
-  const s={
-      estilofig:{
-          heigth:"100px",
-          width:"200px",
-          marginTop:"50px",
-      },
-      estiloicon:{
-          marginLeft:"1rem",
-          fontSize:"1.5rem",
-      },
-  }
+}
+
+export default Cards
+const s={
+    estilofig:{
+        heigth:"100px",
+        width:"200px",
+        marginTop:"50px",
+    },
+    estiloicon:{
+        marginLeft:"1rem",
+        fontSize:"1.5rem",
+    },
+}
