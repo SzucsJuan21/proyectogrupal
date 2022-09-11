ae5a917cff67c653b2d51c16e7ddd40b24045839
juani-abal <juanabal21@gmail.com>
2022-09-11 20:06:38 -0300

    Se le corrigió el responsive a Banner Info

--- src/Componentes/Cards/Section.js
+++ src/Componentes/Cards/Section.js
@@ -8,11 +8,7 @@ const Section = ({ data, dispatch }) => {
   const { products, cart } = data;
 
   const addToCart = async (id) => {
-<<<<<<< HEAD
-    dispatch({ type: TYPES.ADD_TO_CART, payload: id });
-=======
     
->>>>>>> 93e827e1941a8aaef32150c2e9372be528320441
 
     const addedItem = products.find((item) => item.id === id);
     const itemIncart = cart.find((item) => item.id === id);
@@ -25,11 +21,7 @@ const Section = ({ data, dispatch }) => {
       };
 
       let res = await axios(`http://localhost:3001/cart/${id}`, options);
-<<<<<<< HEAD
-      let item = await res.data;
-=======
       if (res.status >= 200 && res.status < 300) dispatch({ type: TYPES.ADD_TO_CART, payload: id });
->>>>>>> 93e827e1941a8aaef32150c2e9372be528320441
     } else {
       let options = {
         method: "POST",
@@ -38,39 +30,11 @@ const Section = ({ data, dispatch }) => {
       };
 
       let res = await axios("http://localhost:3001/cart", options);
-<<<<<<< HEAD
-      let item = await res.data;
-=======
       if (res.status >= 200 && res.status < 300) dispatch({ type: TYPES.ADD_TO_CART, payload: id });
->>>>>>> 93e827e1941a8aaef32150c2e9372be528320441
     }
   };
 
   return (
-<<<<<<< HEAD
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <div>
-        <h1 style={{ fontSize: "24px", fontFamily: "Poppins", marginBottom:0 }}>
-          Ultimas <span style={{color:'#ff8126'}}>Ofertas</span>
-        </h1>
-      </div>
-      <div className="cards-container">
-        {products.map(
-          (product) =>
-            product.id < 5 && (
-              <Cards key={product.id} data={product} addToCart={addToCart} />
-            )
-        )}
-      </div>
-    </div>
-=======
     <>
       {products.length > 0 && (
         <div
@@ -109,7 +73,6 @@ const Section = ({ data, dispatch }) => {
         </div>
       )}
     </>
->>>>>>> 93e827e1941a8aaef32150c2e9372be528320441
   );
 };
 
